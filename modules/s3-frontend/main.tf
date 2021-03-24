5d2bc08a2bceb9351e462449c2753d384f319457
Nick Paz <nicholasmpaz@gmail.com>
2021-03-24 16:43:26 -0700

    Add main.tf

---
+++ modules/s3-frontend/main.tf
@@ -0,0 +1,80 @@
+resource "aws_s3_bucket" "default" {
+  bucket = var.domain_name
+  acl    = "public-read"
+  policy = <<POLICY
+{
+  "Version":"2012-10-17",
+  "Statement":[
+    {
+      "Sid":"AddPermissions",
+      "Effect":"Allow",
+      "Principal": "*",
+      "Action":["s3:GetObject"],
+      "Resource":["arn:aws:s3:::${var.domain_name}/*"]
+    }
+  ]
+}
+POLICY
+  website {
+    index_document = var.index_document
+    error_document = var.index_document
+  }
+}
+
+resource "aws_cloudfront_distribution" "default" {
+  origin {
+    domain_name = aws_s3_bucket.default.bucket_regional_domain_name
+    origin_id   = var.domain_name
+  }
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = var.index_document
+  aliases             = [var.domain_name]
+
+  custom_error_response {
+    error_code         = 404
+    response_code      = 200
+    response_page_path = "/${var.index_document}"
+  }
+
+  default_cache_behavior {
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = true
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = var.domain_name
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = var.domain_certificate_arn
+    minimum_protocol_version = "TLSv1.2_2019"
+    ssl_support_method       = "sni-only"
+  }
+}
+
+resource "aws_route53_record" "default" {
+  zone_id = var.hosted_zone_id
+  name    = var.domain_name
+  type    = "A"
+  alias {
+    name                   = aws_cloudfront_distribution.default.domain_name
+    zone_id                = aws_cloudfront_distribution.default.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
