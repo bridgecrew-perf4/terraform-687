306e16c0e477e77bc18165710eca09d701cb7f78
Nick Paz <nicholasmpaz@gmail.com>
2021-03-24 17:20:01 -0700

    Add variables.tf

--- live/react-boids/main.tf
+++ live/react-boids/main.tf
@@ -12,7 +12,10 @@ provider "aws" {
 
 module "frontend" {
   source                 = "../../modules/s3-frontend"
-  domain_name            = "boids.nickpaz.com"
-  domain_certificate_arn = "arn:aws:acm:us-east-1:060074084645:certificate/4b6877fe-49ea-4ca6-98d4-30d5500743f7"
-  hosted_zone_id         = "Z03949441Z4HA0AXZLD7G"
+  domain_name            = var.domain_name
+  domain_certificate_arn = var.domain_certificate_arn
+  hosted_zone_id         = var.hosted_zone_id
 }
+
+
+
